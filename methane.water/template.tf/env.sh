8ba3690913bd1ebd19d13867e28269f994acaff9
Han Wang <han.wang@fu-berlin.de>
2013-01-22 16:23:07 +0100

    add template for tf calculation

---
+++ methane.water/template.tf/env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#gromacs_install_dir=/home/wanghan/study/thermo.convection/local.inhomo.thermostat
+
+votca_install_dir=~/study/chemical.potential/methane.water/local
+source $votca_install_dir/bin/VOTCARC.bash
+
+gromacs_install_dir=~/local.g4.6
+source $gromacs_install_dir/bin/GMXRC.bash
+
---
+++ methane.water/template.tf/parameters.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+base_conf=tools/gen.conf/spc216.gro
+n_base_block="4 2 2"
+number_density=33.286
+cg_pot_file=/home/cocktail/wanghan/study/thermo.convection/results.mu/caltf.H.wca/tools/table_CG_CG.xvg
+
+# gen.tf.sh parameters
+gmx_nsteps=200000
+gmx_nstenergy=50
+gmx_nstxtcout=50
+
+ex_region_r=0.5
+hy_region_r=2.75
+tf_extension=0.05
+tf_step=0.01
+tf_spline_extension=0.1
+tf_spline_step=0.42142857142857142857
+tf_prefactor=0.039
+
+tf_iterations_max=20
+
+# run.tf.sh parameters
+#run_dir=./run
+tf_file=tabletf.xvg
+dt=0.002
+nsteps=500000
+nstxout=0
+nstvout=0
+nstenergy=100
+nstxtcout=1000
+
