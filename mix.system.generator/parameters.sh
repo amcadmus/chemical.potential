25c69d77a64917a7ab6475cf983b7d856bc72a8f
becwangh <becwangh@bicegate0.int.hlrn.de>
2013-05-21 16:42:03 +0200

    methane in water system works

--- mix.system.generator/parameters.sh
+++ mix.system.generator/parameters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mol1_name=SOL
-mol2_name=MeOH
+mol2_name=Meth
 cg1_charge_group_name=CMW
 cg2_charge_group_name=CMC
 ex1_charge_group_name=EXW
@@ -17,9 +17,9 @@ atom1_itp_file="tops/spc.itp"			#
 adress1_itp_file="tops/spc.adress.itp"		# the COM top should be included!!
 cg1_itp_file="tops/spc.cg.itp"			# 
 
-atom2_itp_file="tops/methanol.itp"		# 
-adress2_itp_file="tops/methanol.adress.itp"	# the COM top should be included!!
-cg2_itp_file="tops/methanol.cg.itp"		# 
+atom2_itp_file="tops/methane.itp"		# 
+adress2_itp_file="tops/methane.adress.itp"	# the COM top should be included!!
+cg2_itp_file="tops/methane.cg.itp"		# 
 
 block_conf_file="confs/*.gro"		# building block of conf
 input_conf_file="confs/input*.gro"		# input conf for productive run
